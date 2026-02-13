2d0c599affcc31c96e2b6d2737a19a6211543ab5
kumudha2029 <kumudha2029@gmail.com>
2026-02-13 13:09:22 +0530

    patient side4

--- careops-frontend/src/pages/PublicClinic.jsx
+++ careops-frontend/src/pages/PublicClinic.jsx
@@ -438,22 +438,3 @@ const FooterText = styled.p`
 `;  import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const BASE_URL = "https://careops-ixxy.onrender.com";
-
-export default function Verify() {
-  const { token } = useParams();
-  const [message, setMessage] = useState("Verifying...");
-
-  useEffect(() => {
-    fetch(`${BASE_URL}/public/verify/${token}`)
-      .then(res => res.json())
-      .then(data => setMessage(data.message));
-  }, [token]);
-
-  return (
-    <div style={{ padding: 50, textAlign: "center" }}>
-      <h1>{message}</h1>
-    </div>
-  );
-}
- 
